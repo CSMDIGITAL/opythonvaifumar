c11126138edc3a74cbab490307c4a40084b3981d
CSMDIGITAL <contato.csmd@gmail.com>
2025-09-25 19:32:08 -0300

    dasafio1.1

---
+++ desafio1.py
@@ -0,0 +1,19 @@
+"""comentarios de multiplas
+linhas
+"""
+# comentarios de uma linha
+
+
+"""
+Para rodar esse programa
+Crie uma venv (ambiente virtual)
+
+python -m venv venv
+
+primeira venv obrigatória
+segunda venv (você decide)
+"""
+
+
+# saida de dados
+print("Hello World")
