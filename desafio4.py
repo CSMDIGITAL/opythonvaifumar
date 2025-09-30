6949e457f4c02808c170dc88becad113b67003ba
Cassio Misquita <137828290+CSMDIGITAL@users.noreply.github.com>
2025-09-30 15:59:40 -0300

    Create desafio4.py

---
+++ desafio4.py
@@ -0,0 +1,27 @@
+# FIXME: MULTIPLATAFORMA
+
+# uso do Python no Windows, Linuxl, macOS
+
+# FIXME: Usos
+
+# Console, Web, Data Science,Embarcados
+
+# FIXME: Identação
+x = -5
+if x > 0:
+    print("verdadeiro é maior que 0")
+else:
+    print("falso , nao [e] maior que zero")
+
+
+# FIXME: Tipagem Dinamica
+
+# int  str float complex
+
+# FIXME? bibliotecas
+
+# import
+
+"""
+  comentarios
+"""
