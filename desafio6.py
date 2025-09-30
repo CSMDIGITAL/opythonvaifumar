4b468938ba605d16c882e7d4b994df207756677b
Cassio Misquita <137828290+CSMDIGITAL@users.noreply.github.com>
2025-09-30 16:01:19 -0300

    Create desafio6.py

---
+++ desafio6.py
@@ -0,0 +1,10 @@
+area_do_campo = 6000
+m2_m2 = 1000000
+area_desmatada = float(input("digite a área de desmatada"))
+area_desmatada = area_desmatada * m2_m2
+campos_exatos = area_desmatada/area_do_campo
+print(area_desmatada)
+print(area_do_campo)
+print(f'{campos_exatos:,.2f}')
+import math
+print(math.sqrt(2))
